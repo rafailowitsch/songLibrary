08ef645f8924c256478adc9314dbc47bc5531acc
rafailwotisch <rafailowitsch@proton.me>
2024-10-14 23:49:14 +0300

    README.md

---
+++ README.md
@@ -0,0 +1,116 @@
+### Описание
+
+Приложение представляет собой реализацию онлайн-библиотеки песен. Оно поддерживает следующие REST методы:
+
+- Получение данных библиотеки с фильтрацией по всем полям и пагинацией.
+- Получение текста песни с пагинацией по куплетам.
+- Удаление песни.
+- Изменение данных песни.
+- Добавление новой песни.
+
+**Технологический стек:** Go (Golang), Chi, PostgreSQL, Redis, Docker.
+
+В корневой папке находится директория `mockMusicInfo`, которая содержит моковое стороннее API, с которым взаимодействует основное приложение при добавлении песни. В 30% случаев это API возвращает ошибку `BadRequest`, чтобы симулировать отсутствие песни в базе данных. Это API запускается командой:
+
+```sh
+go run mockMusicInfo/mockMusicInfo.go
+```
+
+### Настройка
+
+Для запуска приложения необходимо создать `.env` файл со следующими переменными:
+
+```plaintext
+POSTGRES_PASSWORD=пароль для PostgreSQL
+REDIS_PASSWORD=пароль для Redis
+CONFIG_PATH=путь до конфигурационного файла
+```
+
+После настройки конфигурации Swagger будет доступен по адресу: [http://localhost:8089/swagger/](http://localhost:8089/swagger/).
+
+### Миграции
+
+Для применения или отката миграций воспользуйтесь следующими командами (таблица `songs` создаётся автоматически при запуске приложения через миграции):
+
+- **Применение одной миграции:**
+
+```sh
+migrate -path ./internal/app/migrations -database 'postgres://postgres:postgres@localhost:5434/postgres?sslmode=disable' up 1
+```
+
+- **Откат миграции:**
+
+```sh
+migrate -path ./internal/app/migrations -database 'postgres://postgres:postgres@localhost:5434/postgres?sslmode=disable' down 1
+```
+
+## Запуск и использование приложения
+
+Выполните следующие команды для сборки и запуска приложения:
+
+```sh
+docker-compose up --build -d
+go run cmd/main.go
+```
+
+### Примеры использования API
+
+#### POST: /songs
+
+Добавляет новую песню в библиотеку.
+
+**Пример запроса:**
+
+```sh
+curl -X POST localhost:8089/songs/ -H "Content-Type: application/json" -d '{
+    "name": "Mr. Blue Sky",
+    "group": "ELO"
+}'
+```
+
+#### GET: /songs
+
+Получает список всех песен с возможностью фильтрации по параметрам.
+
+**Пример запроса:**
+
+```sh
+curl -X GET localhost:8089/songs?group=elo -H "Content-Type: application/json"
+```
+
+**Пример ответа:**
+
+```json
+[
+    {
+        "id": "51ee20ca-35a3-4da6-9111-b796b56adfb2",
+        "name": "Mr. Blue Sky",
+        "group": "ELO",
+        "text": "Ooh baby, can you hear me moan?",
+        "link": "https://example.com/song4",
+        "release_date": "2024-10-14T00:00:00Z",
+        "created_at": "2024-10-14T23:36:29.170294Z",
+        "updated_at": "2024-10-14T23:36:29.170294Z"
+    },
+    {
+        "id": "fe88a8db-fbd0-47e4-805c-c1293f5b79b8",
+        "name": "Evil Woman",
+        "group": "ELO",
+        "text": "Ooh baby, don't you know I suffer?",
+        "link": "https://example.com/song3",
+        "release_date": "2024-10-14T00:00:00Z",
+        "created_at": "2024-10-14T23:36:46.418175Z",
+        "updated_at": "2024-10-14T23:36:46.418175Z"
+    },
+    {
+        "id": "52f26279-c8e1-4e71-9482-a7e105c3d35a",
+        "name": "Evil Woman",
+        "group": "ELO",
+        "text": "You caught me under false pretenses.",
+        "link": "https://example.com/song4",
+        "release_date": "2024-10-14T00:00:00Z",
+        "created_at": "2024-10-14T23:37:40.34581Z",
+        "updated_at": "2024-10-14T23:37:40.34581Z"
+    }
+]
+```
--- internal/delivery/http/deliveryHttp_test.go
+++ internal/delivery/http/deliveryHttp_test.go
@@ -4,17 +4,19 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"io"
 	"log/slog"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	handler "songLibrary/internal/delivery/http"
 	"songLibrary/internal/delivery/http/mocks"
 	"songLibrary/internal/domain"
+	"songLibrary/internal/dto"
 	"songLibrary/pkg/logger/handlers/slogdiscard"
+	"strings"
 	"testing"
-	"time"
 
-	"github.com/go-chi/chi/v5"
 	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
@@ -29,7 +31,7 @@ func TestAddSong_Success(t *testing.T) {
 
 	h := handler.NewHandler(mockService, mockLog)
 
-	reqBody := handler.SongJSON{
+	reqBody := dto.AddSongRequest{
 		Name:  "Hysteria",
 		Group: "Muse",
 	}
@@ -117,7 +119,7 @@ func TestAddSong_Failure_ServiceError(t *testing.T) {
 
 	h := handler.NewHandler(mockService, mockLog)
 
-	reqBody := handler.SongJSON{
+	reqBody := dto.AddSongRequest{
 		Name:  "Hysteria",
 		Group: "Muse",
 	}
@@ -145,351 +147,52 @@ func TestAddSong_Failure_ServiceError(t *testing.T) {
 	assert.Equal(t, "internal error", respBody["error"])
 }
 
-func TestUpdateSong_Success(t *testing.T) {
+func TestHandler_Update(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	mockService := mocks.NewMockService(ctrl)
-	mockLog := slog.New(slogdiscard.NewDiscardHandler())
+	logger := slog.New(slog.NewTextHandler(os.Stdout))
 
-	h := handler.NewHandler(mockService, mockLog)
-
+	handler := NewHandler(mockService, logger)
 	songID := uuid.New()
-	reqBody := handler.SongJSON{
-		Name:  "Updated Song",
-		Group: "Muse",
-	}
-	reqBodyBytes, _ := json.Marshal(reqBody)
-
-	req := httptest.NewRequest(http.MethodPut, "/songs/"+songID.String(), bytes.NewReader(reqBodyBytes))
-	req.Header.Set("Content-Type", "application/json")
+	reqBody := `{"name": "Updated Song", "group": "Updated Group"}`
+	req := httptest.NewRequest(http.MethodPut, "/songs/"+songID.String(), strings.NewReader(reqBody))
+	req = mux.SetURLVars(req, map[string]string{"id": songID.String()})
 	w := httptest.NewRecorder()
 
-	mockService.EXPECT().Update(gomock.Any(), &domain.SongInfo{
-		ID: songID,
-	}, gomock.Any()).Return(nil)
+	mockService.EXPECT().Update(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
 
-	h.Update(w, req)
+	handler.Update(w, req)
 
 	resp := w.Result()
-	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
 
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
-
-	var respBody map[string]string
-	err := json.NewDecoder(resp.Body).Decode(&respBody)
-	assert.NoError(t, err)
-	assert.Equal(t, "song updated successfully", respBody["message"])
+	assert.Contains(t, string(body), "song updated successfully")
 }
 
-func TestUpdateSong_InvalidID(t *testing.T) {
+func TestHandler_Update_NotFound(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	mockService := mocks.NewMockService(ctrl)
-	mockLog := slog.New(slogdiscard.NewDiscardHandler())
+	logger := slog.New(slog.NewTextHandler(os.Stdout))
 
-	h := handler.NewHandler(mockService, mockLog)
-
-	req := httptest.NewRequest(http.MethodPut, "/songs/invalid-uuid", bytes.NewBuffer([]byte(`{"name": "Updated Song", "group": "Muse"}`)))
-	w := httptest.NewRecorder()
-
-	h.Update(w, req)
-
-	resp := w.Result()
-	defer resp.Body.Close()
-
-	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-
-	var respBody map[string]string
-	err := json.NewDecoder(resp.Body).Decode(&respBody)
-	assert.NoError(t, err)
-	assert.Equal(t, "invalid song id", respBody["error"])
-}
-
-// func TestUpdateSong_NotFound(t *testing.T) {
-// 	ctrl := gomock.NewController(t)
-// 	defer ctrl.Finish()
-
-// 	mockService := mocks.NewMockService(ctrl)
-// 	mockLog := slog.New(slogdiscard.NewDiscardHandler())
-
-// 	h := handler.NewHandler(mockService, mockLog)
-
-// 	songID := uuid.New()
-
-// 	reqBody := handler.SongJSON{
-// 		Name:  "Updated Song",
-// 		Group: "Muse",
-// 	}
-// 	reqBodyBytes, _ := json.Marshal(reqBody)
-
-// 	req := httptest.NewRequest(http.MethodPut, "/songs/"+songID.String(), bytes.NewReader(reqBodyBytes))
-// 	w := httptest.NewRecorder()
-
-// 	mockService.EXPECT().Update(gomock.Any(), gomock.Any(), gomock.Any()).Return(domain.ErrSongNotFound)
-
-// 	h.Update(w, req)
-
-// 	resp := w.Result()
-// 	defer resp.Body.Close()
-
-// 	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
-
-// 	var respBody map[string]string
-// 	err := json.NewDecoder(resp.Body).Decode(&respBody)
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, "song not found", respBody["error"])
-// }
-
-func TestGetAllWithFilter_Success(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockService := mocks.NewMockService(ctrl)
-	mockLog := slog.New(slogdiscard.NewDiscardHandler())
-
-	h := handler.NewHandler(mockService, mockLog)
-
-	req := httptest.NewRequest(http.MethodGet, "/songs?page=1&page_size=2&name=Hysteria&group=Muse", nil)
-	w := httptest.NewRecorder()
-
-	expectedSongs := []*domain.Song{
-		{
-			ID:          uuid.New(),
-			Name:        "Hysteria",
-			Group:       "Muse",
-			Text:        "Ooh baby, don't you know I suffer?",
-			Link:        "http://example.com",
-			ReleaseDate: time.Now(),
-			CreatedAt:   time.Now(),
-			UpdatedAt:   time.Now(),
-		},
-	}
-
-	// Создаем Song объект с данными, как указано в запросе
-	expectedSongFilter := &domain.Song{
-		Name:  "Hysteria",
-		Group: "Muse",
-	}
-
-	mockService.EXPECT().GetAllWithFilter(gomock.Any(), expectedSongFilter, 1, 2).Return(expectedSongs, nil)
-
-	h.GetAllWithFilter(w, req)
-
-	resp := w.Result()
-	defer resp.Body.Close()
-
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-
-	var songsResponse []handler.SongJSON
-	err := json.NewDecoder(resp.Body).Decode(&songsResponse)
-	assert.NoError(t, err)
-	assert.Equal(t, 1, len(songsResponse))
-	assert.Equal(t, "Hysteria", songsResponse[0].Name)
-	assert.Equal(t, "Muse", songsResponse[0].Group)
-}
-
-func TestGetAllWithFilter_InvalidPageSize(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockService := mocks.NewMockService(ctrl)
-	mockLog := slog.New(slogdiscard.NewDiscardHandler())
-
-	h := handler.NewHandler(mockService, mockLog)
-
-	req := httptest.NewRequest(http.MethodGet, "/songs?page=1&page_size=-1", nil)
-	w := httptest.NewRecorder()
-
-	h.GetAllWithFilter(w, req)
-
-	resp := w.Result()
-	defer resp.Body.Close()
-
-	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-
-	var respBody map[string]string
-	err := json.NewDecoder(resp.Body).Decode(&respBody)
-	assert.NoError(t, err)
-	assert.Equal(t, "invalid page_size parameter", respBody["error"])
-}
-
-func TestGetAllWithFilter_InvalidDate(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockService := mocks.NewMockService(ctrl)
-	mockLog := slog.New(slogdiscard.NewDiscardHandler())
-
-	h := handler.NewHandler(mockService, mockLog)
-
-	// Некорректный формат даты (неправильный порядок: день перед месяцем)
-	req := httptest.NewRequest(http.MethodGet, "/songs?release_date=2024-13-01", nil)
-	w := httptest.NewRecorder()
-
-	h.GetAllWithFilter(w, req)
-
-	resp := w.Result()
-	defer resp.Body.Close()
-
-	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-
-	var respBody map[string]string
-	err := json.NewDecoder(resp.Body).Decode(&respBody)
-	assert.NoError(t, err)
-	assert.Equal(t, "invalid release_date parameter", respBody["error"])
-}
-
-func TestDeleteSong_Success(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockService := mocks.NewMockService(ctrl)
-	mockLog := slog.New(slogdiscard.NewDiscardHandler())
-
-	h := handler.NewHandler(mockService, mockLog)
-
-	// Создаем маршрутизатор и регистрируем маршруты
-	r := chi.NewRouter()
-	r.Delete("/songs/{id}", h.Delete)
-
+	handler := NewHandler(mockService, logger)
 	songID := uuid.New()
-
-	req := httptest.NewRequest(http.MethodDelete, "/songs/"+songID.String(), nil)
+	reqBody := `{"name": "Updated Song", "group": "Updated Group"}`
+	req := httptest.NewRequest(http.MethodPut, "/songs/"+songID.String(), strings.NewReader(reqBody))
+	req = mux.SetURLVars(req, map[string]string{"id": songID.String()})
 	w := httptest.NewRecorder()
 
-	mockService.EXPECT().Delete(gomock.Any(), &domain.SongInfo{
-		ID: songID,
-	}).Return(nil)
+	mockService.EXPECT().Update(gomock.Any(), gomock.Any(), gomock.Any()).Return(domain.ErrSongNotFound)
 
-	r.ServeHTTP(w, req)
+	handler.Update(w, req)
 
 	resp := w.Result()
-	defer resp.Body.Close()
-
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-
-	var respBody map[string]string
-	err := json.NewDecoder(resp.Body).Decode(&respBody)
-	assert.NoError(t, err)
-	assert.Equal(t, "song deleted successfully", respBody["message"])
-}
-
-func TestDeleteSong_SongNotFound(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockService := mocks.NewMockService(ctrl)
-	mockLog := slog.New(slogdiscard.NewDiscardHandler())
-
-	// Инициализация обработчика и маршрутизатора
-	h := handler.NewHandler(mockService, mockLog)
-	r := chi.NewRouter()
-	r.Delete("/songs/{id}", h.Delete)
-
-	songID := uuid.New()
-
-	// Запрос для удаления
-	req := httptest.NewRequest(http.MethodDelete, "/songs/"+songID.String(), nil)
-	w := httptest.NewRecorder()
-
-	// Ожидание вызова метода Delete с ошибкой "song not found"
-	mockService.EXPECT().Delete(gomock.Any(), &domain.SongInfo{
-		ID: songID,
-	}).Return(domain.ErrSongNotFound)
-
-	// Выполнение запроса через маршрутизатор
-	r.ServeHTTP(w, req)
-
-	// Проверка ответа
-	resp := w.Result()
-	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
 
 	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
-
-	var respBody map[string]string
-	err := json.NewDecoder(resp.Body).Decode(&respBody)
-	assert.NoError(t, err)
-	assert.Equal(t, "song not found", respBody["error"])
-}
-
-func TestGetPaginatedText_Success(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockService := mocks.NewMockService(ctrl)
-	mockLog := slog.New(slogdiscard.NewDiscardHandler())
-
-	// Инициализация обработчика и маршрутизатора
-	h := handler.NewHandler(mockService, mockLog)
-	r := chi.NewRouter()
-	r.Get("/songs/{id}/text", h.GetPaginatedText)
-
-	songID := uuid.New()
-	expectedVerses := []string{
-		"Ooh baby, don't you know I suffer?",
-		"Ooh baby, can you hear me moan?",
-	}
-
-	// Запрос для получения пагинированного текста
-	req := httptest.NewRequest(http.MethodGet, "/songs/"+songID.String()+"/text", nil)
-	w := httptest.NewRecorder()
-
-	// Ожидание вызова метода GetPaginatedText
-	mockService.EXPECT().GetPaginatedText(gomock.Any(), &domain.SongInfo{
-		ID: songID,
-	}).Return(expectedVerses, nil)
-
-	// Выполнение запроса через маршрутизатор
-	r.ServeHTTP(w, req)
-
-	// Проверка ответа
-	resp := w.Result()
-	defer resp.Body.Close()
-
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-
-	var respBody handler.PaginatedTextResponse
-	err := json.NewDecoder(resp.Body).Decode(&respBody)
-	assert.NoError(t, err)
-	assert.Equal(t, expectedVerses, respBody.Text)
-}
-
-func TestGetPaginatedText_SongNotFound(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockService := mocks.NewMockService(ctrl)
-	mockLog := slog.New(slogdiscard.NewDiscardHandler())
-
-	// Инициализация обработчика и маршрутизатора
-	h := handler.NewHandler(mockService, mockLog)
-	r := chi.NewRouter()
-	r.Get("/songs/{id}/text", h.GetPaginatedText)
-
-	songID := uuid.New()
-
-	// Запрос для получения пагинированного текста
-	req := httptest.NewRequest(http.MethodGet, "/songs/"+songID.String()+"/text", nil)
-	w := httptest.NewRecorder()
-
-	// Ожидание вызова метода GetPaginatedText с ошибкой "song not found"
-	mockService.EXPECT().GetPaginatedText(gomock.Any(), &domain.SongInfo{
-		ID: songID,
-	}).Return(nil, domain.ErrSongNotFound)
-
-	// Выполнение запроса через маршрутизатор
-	r.ServeHTTP(w, req)
-
-	// Проверка ответа
-	resp := w.Result()
-	defer resp.Body.Close()
-
-	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
-
-	var respBody map[string]string
-	err := json.NewDecoder(resp.Body).Decode(&respBody)
-	assert.NoError(t, err)
-	assert.Equal(t, "song not found", respBody["error"])
+	assert.Contains(t, string(body), "song not found")
 }
